2d35599dfe81aedaa5e78f3367e9884df971e8d4
roman <romanze1706@gmail.com>
2023-04-10 15:50:24 +0300

    Создана ветка dev

--- src/main/java/ru/skypro/homework/ShopApplication.java
+++ src/main/java/ru/skypro/homework/ShopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShopApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ShopApplication.class, args);
     }
 
