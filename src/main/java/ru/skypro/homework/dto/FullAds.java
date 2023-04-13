0047a8a7142e9ed810c2a7a83b568b6842e31b4d
roman <romanze1706@gmail.com>
2023-04-13 18:43:49 +0300

    ...

---
+++ src/main/java/ru/skypro/homework/dto/FullAds.java
@@ -0,0 +1,23 @@
+package ru.skypro.homework.dto;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class FullAds {
+
+    // id объявления
+    int pk;
+    String authorFirstName;
+    String authorLastName;
+    String description;
+    String email;
+    // url image
+    String image;
+    String phone;
+    int price;
+    String title;
+
+}
--- src/main/java/ru/skypro/homework/dto/ResponseWrapperComment.java
+++ src/main/java/ru/skypro/homework/dto/ResponseWrapperComment.java
@@ -12,5 +12,6 @@ public class ResponseWrapperComment {
 
     //Общее количество комментариев
     int count;
+    List<Comment> results;
 
 }
