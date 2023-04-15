f1dcdb232297d796f1010688f326246fe107b7ca
roman <romanze1706@gmail.com>
2023-04-15 13:07:14 +0300

    Передалал немного spring security

--- src/main/java/ru/skypro/homework/controller/BasicAuthCorsFilter.java
+++ src/main/java/ru/skypro/homework/controller/BasicAuthCorsFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-@Component
+//@Component
 public class BasicAuthCorsFilter extends OncePerRequestFilter {
 
     @Override
