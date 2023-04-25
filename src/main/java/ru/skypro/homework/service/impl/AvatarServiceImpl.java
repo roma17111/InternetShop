94b26333c57766cc642212a607909f3920a912c3
roman <romanze1706@gmail.com>
2023-04-25 12:00:50 +0300

    Доработки в AvatarServiceImpl.java

--- src/main/java/ru/skypro/homework/service/impl/AvatarServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AvatarServiceImpl.java
@@ -2,18 +2,22 @@ package ru.skypro.homework.service.impl;
 
 import com.jcraft.jsch.*;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.models.Avatar;
 import ru.skypro.homework.service.AvatarService;
 import ru.skypro.homework.service.repository.AvatarRepository;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -24,24 +28,25 @@ import java.util.concurrent.*;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@EnableAsync
 public class AvatarServiceImpl implements AvatarService {
 
     private final AvatarRepository avatarRepository;
 
     @Value("${sftp.host}")
-    private String sftpHost;
+    private String SFTP_HOST;
 
     @Value("${sftp.port}")
-    private int sftpPort;
+    private int SFTP_PORT;
 
     @Value("${sftp.user}")
-    private String sftpUser;
+    private String SFTP_USER;
 
     @Value("${sftp.password}")
-    private String sftpPassword;
+    private String SFTP_PASSWORD;
 
     @Value("${sftp.directory}")
-    private String sftpDirectory;
+    private String SFTP_DIRECTORY;
 
     @Override
     public List<Avatar> getAllAvatars() {
@@ -50,34 +55,23 @@ public class AvatarServiceImpl implements AvatarService {
 
     @Override
     public void testSave(MultipartFile file, MediaType mediaType) {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Future<?> future = executor.submit(() -> {
-            ChannelSftp channelSftp = null;
-            try {
-                channelSftp = setupJsch();
-                InputStream inputStream = file.getInputStream();
-                String filePath = sftpDirectory + file.getOriginalFilename();
-                channelSftp.put(inputStream, filePath);
-                Avatar avatar = new Avatar();
-                avatar.setFileSize((int) file.getSize());
-                avatar.setImageType(mediaType.toString());
-                avatar.setAvatarPath(filePath);
-                avatarRepository.save(avatar);
-            } catch (Exception e) {
-                log.error(e.getMessage());
-            } finally {
-                if (channelSftp != null) {
-                    channelSftp.disconnect();
-                }
-            }
-        });
-
+        ChannelSftp channelSftp = null;
         try {
-            future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
+            channelSftp = setupJsch();
+            InputStream inputStream = file.getInputStream();
+            String filePath = SFTP_DIRECTORY + file.getOriginalFilename();
+            channelSftp.put(inputStream, filePath);
+            Avatar avatar = new Avatar();
+            avatar.setFileSize((int) file.getSize());
+            avatar.setImageType(mediaType.toString());
+            avatar.setAvatarPath(filePath);
+            avatarRepository.save(avatar);
+        } catch (Exception e) {
+            log.error(e.getMessage());
         } finally {
-            executor.shutdown();
+            if (channelSftp != null) {
+                channelSftp.disconnect();
+            }
         }
     }
 
@@ -85,29 +79,17 @@ public class AvatarServiceImpl implements AvatarService {
     public byte[] getAvatarImage(long avatarId) {
         Avatar avatar = avatarRepository.findById(avatarId).orElseThrow(() -> new RuntimeException("Avatar not found"));
         String avatarPath = avatar.getAvatarPath();
-
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Future<byte[]> future = executor.submit(() -> {
-            ChannelSftp sftpChannel = null;
-            try {
-                sftpChannel = setupJsch();
-                InputStream inputStream = sftpChannel.get(avatarPath);
-                return IOUtils.toByteArray(inputStream);
-            } catch (JSchException | SftpException | IOException e) {
-                throw new RuntimeException(e);
-            } finally {
-                if (sftpChannel != null) {
-                    sftpChannel.exit();
-                }
-            }
-        });
-
+        ChannelSftp sftpChannel = null;
         try {
-            return future.get();
-        } catch (InterruptedException | ExecutionException e) {
+            sftpChannel = setupJsch();
+            InputStream inputStream = sftpChannel.get(avatarPath);
+            return IOUtils.toByteArray(inputStream);
+        } catch (JSchException | SftpException | IOException e) {
             throw new RuntimeException(e);
         } finally {
-            executor.shutdown();
+            if (sftpChannel != null) {
+                sftpChannel.exit();
+            }
         }
     }
 
@@ -116,39 +98,39 @@ public class AvatarServiceImpl implements AvatarService {
         Avatar avatar = avatarRepository.findById(avatarId).orElseThrow();
         String avatarPath = avatar.getAvatarPath();
 
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Future<ByteArrayOutputStream> future = executor.submit(() -> {
-            ChannelSftp channelSftp = setupJsch();
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(SFTP_USER, SFTP_HOST, SFTP_PORT);
+        session.setPassword(SFTP_PASSWORD);
+        session.setConfig("StrictHostKeyChecking", "no");
+        session.connect();
 
-            InputStream inputStream = channelSftp.get(avatarPath);
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, len);
-            }
+        ChannelSftp channelSftp = (ChannelSftp) session.openChannel("sftp");
+        channelSftp.connect();
 
-            channelSftp.disconnect();
+        InputStream inputStream = channelSftp.get(avatarPath);
 
-            return outputStream;
-        });
-
-        try {
-            ByteArrayOutputStream outputStream = future.get();
-            byte[] data = outputStream.toByteArray();
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
-            return new InputStreamResource(byteArrayInputStream);
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        } finally {
-            executor.shutdown();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, len);
         }
+        byte[] data = outputStream.toByteArray();
+
+        inputStream.close();
+        outputStream.close();
+        channelSftp.disconnect();
+        session.disconnect();
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+
+        return new InputStreamResource(byteArrayInputStream);
     }
 
     private ChannelSftp setupJsch() throws JSchException {
         JSch jsch = new JSch();
-        Session session = jsch.getSession(sftpUser, sftpHost, sftpPort);
-        session.setPassword(sftpPassword);
+        Session session = jsch.getSession(SFTP_USER, SFTP_HOST, SFTP_PORT);
+        session.setPassword(SFTP_PASSWORD);
         session.setConfig("StrictHostKeyChecking", "no");
         session.connect();
         Channel channel = session.openChannel("sftp");
